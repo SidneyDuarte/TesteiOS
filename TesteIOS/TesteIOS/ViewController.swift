6b9dceeafc3049e80c1a2106753f0a82d92267b4
Sidney Silva <sidney_silva13@outlook.com>
2019-02-03 19:07:04 -0200

    Commit inicial

---
+++ TesteIOS/TesteIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesteIOS
+//
+//  Created by Sidney Silva on 03/02/19.
+//  Copyright © 2019 Sakura Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
